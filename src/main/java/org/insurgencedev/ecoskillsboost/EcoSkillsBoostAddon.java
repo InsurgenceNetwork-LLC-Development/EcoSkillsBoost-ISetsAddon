83c0984a5365a1faacc37baa8fa3b136733914e2
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-21 21:58:17 -0500

    updated api and fixed boosts

--- src/main/java/org/insurgencedev/ecoskillsboost/EcoSkillsBoostAddon.java
+++ src/main/java/org/insurgencedev/ecoskillsboost/EcoSkillsBoostAddon.java
@@ -1,21 +1,17 @@
 package org.insurgencedev.ecoskillsboost;
 
-import org.bukkit.Bukkit;
+import org.insurgencedev.ecoskillsboost.listeners.SkillsExperienceGainListener;
 import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
 import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
 import org.insurgencedev.insurgencesets.libs.fo.Common;
 
-@ISetsAddon(name = "EcoSkillsBoost", version = "1.0.0", author = "Insurgence Dev Team", description = "Boost the skill experience earned from EcoSkills")
+@ISetsAddon(name = "EcoSkillsBoost", version = "1.0.1", author = "Insurgence Dev Team", description = "Boost the skill experience earned from EcoSkills")
 public class EcoSkillsBoostAddon extends InsurgenceSetsAddon {
 
     @Override
     public void onAddonReloadablesStart() {
-        if (isDependentEnabled()) {
+        if (Common.doesPluginExist("EcoSkills")) {
             registerEvent(new SkillsExperienceGainListener());
         }
-    }
-
-    private boolean isDependentEnabled() {
-        return Bukkit.getPluginManager().isPluginEnabled("EcoSkills");
     }
 }
